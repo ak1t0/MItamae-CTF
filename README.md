13f1fbb1093e7bc451cae5e832d25c829bfcb33f
ak1t0 <aktoo3097@gmail.com>
2017-04-19 23:16:54 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# MItamae-CTF
+
+## Description
+This is provisioning file for CTF player based on [MItamae](https://github.com/k0kubun/mitamae).
+
+> MItamae  
+> Itamae implementation that is runnable without Ruby, which is a lightweight configuration management tool inspired by Chef. 
+> With mitamae's standalone binary, you can write a configuration recipe in Ruby and apply it without Ruby.  
+> https://github.com/k0kubun/mitamae
+
+## Development
+- Ubuntu 17.04
+
+## Config
+node.json
+```json
+{
+    "user": "",
+    "dir": ""
+}
+
+```
+sample
+```json
+{
+    "user": "ak1t0",
+    "dir": "Works"
+}
+```
+
+If this json is specified, tools will be installed to `/home/ak1t0/Works/ctf`.  
+dir should be `""` when you don't need dir.
+
+
+## Install
+`sudo ./bin/mitamae local -j node.json recipe.rb`
+
+### Tools
+git, curl, binutils, lib32z1, lib32ncurses5, gdb-peda, enjarify, volatility, wireshark, sleuthkit, binwalk, radare2, jd-gui, stegsolve, rp++, pwntools
+
+This is based on [Mitamae-Debian](https://github.com/taroooyan/mitamae-debian) and [mitamae-osx](https://github.com/upamune/mitamae-osx).
+
+
+Copyright © 2017 ak1t0
+
+Distributed under [LICENSE](https://github.com/ak1t0/Mitamae-CTF/blob/master/LICENSE)
---
+++ cookbooks/ctf/default.rb
@@ -0,0 +1,114 @@
+directory "/home/#{node[:user]}/#{node[:dir]}/ctf" do
+  mode "775"
+  owner "#{node[:user]}"
+  group "#{node[:user]}"
+end
+
+package "git" do
+  user "root"
+  action :install
+end
+
+package "curl" do
+  user "root"
+  action :install
+end
+
+package "binutils" do
+  user "root"
+  action :install
+end
+
+["lib32z1", "lib32ncurses5"].each do |p|
+    package p do
+    user "root"
+    action :install
+  end
+end
+
+git "/home/#{node[:user]}/#{node[:dir]}/ctf/peda" do
+  repository "git://github.com/longld/peda.git"
+end
+
+execute "Install gdb-peda" do
+  user node[:user]
+  command "echo 'source /home/#{node[:user]}/#{node[:dir]}/ctf/peda/peda.py' >> ~/.gdbinit"
+end
+  
+git "/home/#{node[:user]}/#{node[:dir]}/ctf/enjarify" do
+  repository "git://github.com/Storyyeller/enjarify.git"
+end
+
+git "/home/#{node[:user]}/#{node[:dir]}/ctf/volatility" do
+  repository "git://github.com/volatilityfoundation/volatility.git"
+end
+
+package "wireshark" do
+  user "root"
+  action :install
+end
+
+package "sleuthkit" do
+  user "root"
+  action :install
+end
+
+bw_packages = ["python-lzma", "python-crypto", "mtd-utils", "gzip", "bzip2", "tar", "arj", "lhasa", "p7zip", "p7zip-full", "cabextract", "cramfsprogs", "cramfsswap", "squashfs-tools"]
+
+bw_packages.each do |p| 
+  package p do
+    user "root"
+    action :install
+  end
+end
+
+git "/home/#{node[:user]}/#{node[:dir]}/ctf/binwalk" do
+  repository "git://github.com/devttys0/binwalk.git"
+end
+
+execute "Install binwalk" do
+  user "root"
+  command "cd /home/#{node[:user]}/#{node[:dir]}/ctf/binwalk; sudo python setup.py install" 
+end
+
+git "/home/#{node[:user]}/#{node[:dir]}/ctf/radare2" do
+  repository "git://github.com/radare/radare2.git"
+end
+
+execute "Install radare2" do
+  user "root"
+  command "cd /home/#{node[:user]}/#{node[:dir]}/ctf/radare2; sys/install.sh" 
+end
+
+execute "Install JD-GUI" do
+  user node[:user]
+  command "cd /home/#{node[:user]}/#{node[:dir]}/ctf; wget https://github.com/java-decompiler/jd-gui/releases/download/v1.4.0/jd-gui-1.4.0.jar -O jd-gui.jar; chmod +x ./jd-gui.jar"
+end
+
+execute "Install Stegsolve" do
+  user node[:user]
+  command "cd /home/#{node[:user]}/#{node[:dir]}/ctf; wget http://www.caesum.com/handbook/Stegsolve.jar -O stegsolve.jar; chmod +x ./stegsolve.jar"
+end
+
+execute "Install rp++" do
+  user node[:user]
+  command "cd /home/#{node[:user]}/#{node[:dir]}/ctf; wget https://github.com/downloads/0vercl0k/rp/rp-lin-x64 -O rp; chmod +x ./rp"
+end
+
+pt_packages = ["python-pip", "python-dev", "libssl-dev", "libffi-dev"]
+pt_packages.each do |p| 
+  package p do
+    user "root"
+    action :install
+  end
+end
+
+execute "Upgrade pip" do
+  user "root"
+  command "pip install --upgrade pip" 
+end
+
+execute "Install pwntools" do
+  user "root"
+  command "pip install --upgrade pwntools" 
+end
